8249690ca1a1110d97d4e4d0769b9426d4a5953b
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-01 16:55:57 +0100

    created file structure of cinemaDB

---
+++ view/listFIlms.php
@@ -0,0 +1,13 @@
+<?php ob_start(); ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>TITRE</th>
+            <th>ANNEE SORTIE</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
