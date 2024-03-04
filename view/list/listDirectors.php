3907c60615120bb6fe0e7ad4dce3da7c5a8545e7
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-04 16:04:52 +0100

    Added list of directors

---
+++ view/list/listDirectors.php
@@ -0,0 +1,41 @@
+<?php ob_start(); ?>
+
+<div class="wrapper">
+    <h1 class="maintitle">DIRECTORS</h1>
+
+    <div class="options">
+        <form action="controller/CinemaController.php" method="post">
+            <select name="sortby" id="sort1" class="opt">
+                <option value="title">Name</option>
+                <option value="date">Surname</option>
+            </select>
+
+            <select name="orderby" id="order1" class="opt">
+                <option value="asc">Ascendent</option>
+                <option value="desc">Descendent</option>
+            </select>
+        </form>
+
+        <div class="opt">
+            <a href="">Add director</a>
+        </div>
+    </div>
+
+    <table class="list">
+        <tbody>
+            <?php
+                foreach($queryDirectorInfo -> fetchALL() as $director){?>
+                    <tr>
+                        <td><a href=""><?= $director["nomRealisateur"] ?></a></td>
+                    </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+
+<?php
+
+$title= "DIRECTORS";
+$content= ob_get_clean();
+require "view/template.php";
