661fff1c75b9b5fdae46d6eaad9a11c286aec39a
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-04 16:06:26 +0100

    added controller functions for lists

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -4,28 +4,74 @@ namespace Controller;
 use Model\Connect;
 
 class CinemaController{
+    public function homePage(){
+            $pdo = Connect::seConnecter();
+            $queryMovieDiscover = $pdo -> query("
+                SELECT affiche, titre_film, DATE_FORMAT (date_sortie, '%Y') as date, CONCAT(prenom, ' ', nom) AS realisateurFilm
+                FROM film
+                INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+                INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+                LIMIT 5
+            ");
+            $queryPerson = $pdo -> query("
+                SELECT prenom, nom
+                FROM personne
+            ");
+
+            require "view/homePage.php";
+        }
 
     public function listMovies(){
         $pdo = Connect::seConnecter();
         $queryMovieInfo = $pdo -> query("
-            SELECT titre_film, date_sortie
+            SELECT titre_film, DATE_FORMAT (date_sortie, '%Y') as date, CONCAT(prenom, ' ', nom) AS realisateurFilm
             FROM film
+            INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+            INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+            ORDER BY date DESC
         ");
-
         require "view/list/listMovies.php";
     }
 
-    public function homePage(){
+    public function listDirectors(){
         $pdo = Connect::seConnecter();
-        $queryMovieDisplay = $pdo -> query("
-            SELECT affiche, titre_film, date_sortie
-            FROM film
+        $queryDirectorInfo = $pdo -> query("
+            SELECT CONCAT(prenom, ' ', nom) AS nomRealisateur
+            FROM realisateur
+            INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+            ORDER BY nom
         ");
-        $queryPersonDisplay = $pdo -> query("
-            SELECT photo, prenom, nom
-            FROM personne
-        ");
+        require "view/list/listDirectors.php";
+    }
 
-        require "view/homePage.php";
+    public function listActors(){
+        $pdo = Connect::seConnecter();
+        $queryActorInfo = $pdo -> query("
+            SELECT CONCAT(prenom, ' ', nom) AS nomActeur
+            FROM acteur
+            INNER JOIN personne ON acteur.id_personne = personne.id_personne
+            ORDER BY nom
+        ");
+        require "view/list/listActors.php";
+    }
+
+    public function listGenres(){
+        $pdo = Connect::seConnecter();
+        $queryGenreInfo = $pdo -> query("
+            SELECT nom_genre
+            FROM genre
+            ORDER BY nom_genre
+        ");
+        require "view/list/listGenres.php";
+    }
+
+    public function listRoles(){
+        $pdo = Connect::seConnecter();
+        $queryRoleInfo = $pdo -> query("
+            SELECT nom_role
+            FROM role
+            ORDER BY nom_role
+        ");
+        require "view/list/listRoles.php";
     }
 }
