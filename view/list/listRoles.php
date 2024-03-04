4cb9920b894c3295c7d8237e422f19b605fe8d77
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-04 16:05:06 +0100

    Added list of roles

---
+++ view/list/listRoles.php
@@ -0,0 +1,39 @@
+<?php ob_start(); ?>
+
+<div class="wrapper">
+    <h1 class="maintitle">ROLES</h1>
+
+    <div class="options">
+        <form action="controller/CinemaController.php" method="post">
+            <select name="sortby" id="sort1" class="opt">
+                <option value="title">Name</option>
+            </select>
+
+            <select name="orderby" id="order1" class="opt">
+                <option value="asc">Ascendent</option>
+                <option value="desc">Descendent</option>
+            </select>
+        </form>
+
+        <div class="opt">
+            <a href="">Add role</a>
+        </div>
+    </div>
+
+    <table class="list">
+        <tbody>
+            <?php
+                foreach($queryRoleInfo -> fetchALL() as $role){?>
+                    <tr>
+                        <td><a href=""><?= $role["nom_role"] ?></a></td>
+                    </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<?php
+
+$title= "ROLES";
+$content= ob_get_clean();
+require "view/template.php";
