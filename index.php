63ed86db6626b04ccd5af3de9c70de738ee291cc
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-04 16:05:41 +0100

    added cases for list redirects

--- index.php
+++ index.php
@@ -9,10 +9,31 @@ spl_autoload_register(function ($class_name) {
 $ctrlCinema = new CinemaController();
 
 if(isset($_GET["action"])){
+
     switch($_GET["action"]){
+
+        case "homePage" :
+            $ctrlCinema-> homePage();
+            break;
+
+        case "listDirectors" :
+            $ctrlCinema -> listDirectors();
+            break;   
+
+        case "listActors" :
+                $ctrlCinema -> listActors();
+                break;
 
         case "listMovies" :
             $ctrlCinema -> listMovies();
+            break;
+
+        case "listGenres" :
+            $ctrlCinema -> listGenres();
+            break;
+
+        case "listRoles" :
+            $ctrlCinema -> listRoles();
             break;
     }
 } else {
