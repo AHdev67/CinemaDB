e5565b8cea26a55f3fe6e61d45ec3159fe39c190
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-11 16:57:47 +0100

    added add actor form

---
+++ view/addpage/addActor.php
@@ -0,0 +1,51 @@
+<?php
+    ob_start();
+?>
+<div class="container">
+    <div class="title">
+        <h1>ADD NEW ACTOR</h1>
+        <span>(fields with * are mandatory)</span>
+    </div>
+
+    <form action="index.php?action=submitActor" method="post">
+        <label for="firstnameInput">First name* :</label>
+        <input type="text" name="inputFirstname" id="firstnameInput">
+
+        <br>
+
+        <label for="lastnameInput">Last name* :</label>
+        <input type="text" name="inputLastname" id="lastnameInput">
+
+        <br>
+    
+        <label for="DoBInput">Date of birth* :</label>
+        <input type="date" name="inputDoB" id="DoBInput">
+
+        <br>
+
+        <label for="sexInput">Sex :</label>
+        <select name="inputSex" id="scoreInput">
+            <option value="H">Homme</option>
+            <option value="F">Femme</option>
+        </select>
+
+        <br>
+
+        <label for="biographyInput">Biography</label>
+        <input type="text" name="inputBiography" id="biographyInput">
+
+        <br>
+
+        <label for="photoInput">Photo URL :</label>
+        <input type="text" name="inputPhoto" id="photoInput">
+
+        <br>
+
+        <input type="submit" name="submitForm" id="formSubmit" value="Add movie to DB">
+    </form>
+</div>
+
+<?php
+$title= "ADD ACTOR";
+$content= ob_get_clean();
+require "view/template.php";
