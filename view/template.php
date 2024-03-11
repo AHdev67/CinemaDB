7ab2ef7c77804eb01b709d2401b8fc4d5e7345ee
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-11 16:57:24 +0100

    added burger menu, fixed minor things

--- view/template.php
+++ view/template.php
@@ -4,62 +4,92 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b28c0a82b5.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="public/css/main.css">
+    <!-- <link rel="stylesheet" href="/public/css/main.css"> -->
+   <link rel="stylesheet" href="public\css\main.css">
     <title><?= $title ?></title>
 </head>
 <body>
     <!-- NAVBAR -->
     <header>
         <nav>
-            <div class="navbarContent">
-                
-                <span class="navtitle">
+            <div id="navbarContent">
+
+                <div id="navtitle">
                     <a href="index.php?action=homePage">
-                        <div class="navlogo"><i class="fa-solid fa-film"></i></div>
+                        <i id="navlogo" class="fa-solid fa-clapperboard"></i>
                         CINEMA DB
                     </a>
-                </span>
+                </div>
+
+                <div id="search">
+                    <input type="text" placeholder="search" id="searchbar"></input>
+                    <button id="searchbutton">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div> 
                 
-                <div class="searchbar"><i class="fa-solid fa-magnifying-glass"></i></div>
 
-                <div class="burgermenu">
-                    <span class="burgericon"></span>
-                    <span class="burgericon"></span>
-                    <span class="burgericon"></span>
-                </div>
+                <button id="burger">
+                    <i id="menuIcon" class="fa-solid fa-bars"></i>
+                    <i id="closeIcon" class="fa-solid fa-xmark"></i>
+                </button>
 
-                <div class="menu">
-                    <ul>
-                        <li>
-                            <a href="index.php?action=listMovies">MOVIES</a>
-                        </li>
-                        <li>
-                            <a href="index.php?action=listDirectors">DIRECTORS</a>
-                        </li>
-                        <li>
-                            <a href="index.php?action=listActors">ACTORS</a>
-                        </li>
-                        <li>
-                            <a href="index.php?action=listRoles">ROLES</a>
-                        </li>
-                        <li>
-                            <a href="index.php?action=listGenres">GENRES</a>
-                        </li>
-                        <li>
-                            <a href="">CONTACT US</a>
-                        </li>
-                    </ul>
-                </div>
+                <ul id="menu" class="hideMenu">
+
+                    <li>
+                        <a class="menuItem" href="index.php?action=homePage">HOME</a>
+                    </li>                
+                    <li>
+                        <a class="menuItem" href="index.php?action=listMovies">MOVIES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listDirectors">DIRECTORS</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listActors">ACTORS</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listRoles">ROLES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listGenres">GENRES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="">CONTACT US</a>
+                    </li>
+                </ul>
 
             </div>
         </nav>
     </header>
 
     <!-- MAIN -->
-    <main>
+    <main id="wrapper">
         <div id="content">
             <?= $content ?>
         </div>
     </main>
+
+    <script>
+        const menu = document.querySelector("#menu");
+        const menuItems = document.querySelectorAll(".menuItem");
+        const hamburger= document.querySelector("#burger");
+        const closeIcon= document.querySelector("#closeIcon");
+        const menuIcon = document.querySelector("#menuIcon");
+
+        function toggleMenu() {
+            if (menu.classList.contains("hideMenu")) {
+                menu.classList.remove("hideMenu");
+                closeIcon.style.display = "block";
+                menuIcon.style.display = "none";
+            } else {
+                menu.classList.add("hideMenu");
+                closeIcon.style.display = "none";
+                menuIcon.style.display = "block";
+            }
+        }
+
+        hamburger.addEventListener("click", toggleMenu);
+    </script>
 </body>
 </html>
