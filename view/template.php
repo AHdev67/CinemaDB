44f272dc60a17799687527d589e97928e05c3a45
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-12 11:05:33 +0100

    added footer

--- view/template.php
+++ view/template.php
@@ -5,71 +5,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b28c0a82b5.js" crossorigin="anonymous"></script>
     <!-- <link rel="stylesheet" href="/public/css/main.css"> -->
+
    <link rel="stylesheet" href="public\css\main.css">
     <title><?= $title ?></title>
 </head>
+
 <body>
-    <!-- NAVBAR -->
+<!-- ---------------------------------NAVBAR--------------------------------- -->
     <header>
         <nav>
-            <div id="navbarContent">
+            <div id="navtitle">
+                <a href="index.php?action=homePage">
+                    <i id="navlogo" class="fa-solid fa-clapperboard"></i>
+                    CINEMA DB
+                </a>
+            </div>
 
-                <div id="navtitle">
-                    <a href="index.php?action=homePage">
-                        <i id="navlogo" class="fa-solid fa-clapperboard"></i>
-                        CINEMA DB
-                    </a>
-                </div>
-
-                <div id="search">
-                    <input type="text" placeholder="search" id="searchbar"></input>
-                    <button id="searchbutton">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
-                </div> 
+            <div id="search">
+                <input type="text" placeholder="search" id="searchbar"></input>
+                <button id="searchbutton">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div> 
                 
 
-                <button id="burger">
-                    <i id="menuIcon" class="fa-solid fa-bars"></i>
-                    <i id="closeIcon" class="fa-solid fa-xmark"></i>
-                </button>
+            <button id="burger">
+                <i id="menuIcon" class="fa-solid fa-bars"></i>
+                <i id="closeIcon" class="fa-solid fa-xmark"></i>
+            </button>
 
-                <ul id="menu" class="hideMenu">
+            <ul id="menu" class="hideMenu">
 
-                    <li>
-                        <a class="menuItem" href="index.php?action=homePage">HOME</a>
-                    </li>                
-                    <li>
-                        <a class="menuItem" href="index.php?action=listMovies">MOVIES</a>
-                    </li>
-                    <li>
-                        <a class="menuItem" href="index.php?action=listDirectors">DIRECTORS</a>
-                    </li>
-                    <li>
-                        <a class="menuItem" href="index.php?action=listActors">ACTORS</a>
-                    </li>
-                    <li>
-                        <a class="menuItem" href="index.php?action=listRoles">ROLES</a>
-                    </li>
-                    <li>
-                        <a class="menuItem" href="index.php?action=listGenres">GENRES</a>
-                    </li>
-                    <li>
-                        <a class="menuItem" href="">CONTACT US</a>
-                    </li>
-                </ul>
-
-            </div>
+                <li>
+                    <a class="menuItem" href="index.php?action=homePage">HOME</a>
+                </li>                
+                <li>
+                    <a class="menuItem" href="index.php?action=listMovies">MOVIES</a>
+                </li>
+                <li>
+                    <a class="menuItem" href="index.php?action=listDirectors">DIRECTORS</a>
+                </li>
+                <li>
+                    <a class="menuItem" href="index.php?action=listActors">ACTORS</a>
+                </li>
+                <li>
+                    <a class="menuItem" href="index.php?action=listRoles">ROLES</a>
+                </li>
+                <li>
+                    <a class="menuItem" href="index.php?action=listGenres">GENRES</a>
+                </li>
+                <li>
+                    <a class="menuItem" href="">CONTACT US</a>
+                </li>
+            </ul>
         </nav>
     </header>
 
-    <!-- MAIN -->
+<!-- ----------------------------------MAIN---------------------------------- -->
     <main id="wrapper">
         <div id="content">
             <?= $content ?>
         </div>
     </main>
 
+<!-- ---------------------------------FOOTER--------------------------------- -->
+    <footer>
+        <div id="footer1">
+            <h2>CONTACT</h2>
+            <p>
+                <a href="mailto:cinemadb@contact.com">cinemadb@contact.com</a>
+            </p>
+
+            <p>
+                06 83 00 69 42
+            </p>  
+        </div>
+
+        <div id="footer2">
+           <p>
+                <a href="">Q&A</a>
+           </p>
+
+           <p>
+                <a href="">Privacy policy</a>
+           </p>
+        </div>
+
+        <div id="footer3">
+            <h2>FOLLOW US</h2>
+
+            <p>
+                <span id="insta" class="socialIcon">
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
+                </span>
+
+                <span id="twitter" class="socialIcon">
+                    <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+                </span>
+
+                <span id="fb" class="socialIcon">
+                    <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+                </span>
+            </p>
+        </div>
+    </footer>
+
+<!-- ---------------------------SCRIPT MENU BURGER--------------------------- -->
     <script>
         const menu = document.querySelector("#menu");
         const menuItems = document.querySelectorAll(".menuItem");
