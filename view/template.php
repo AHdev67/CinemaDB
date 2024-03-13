fe28a714df268ebcc11f73d1ea81029fde0ee198
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-13 11:51:18 +0100

    updated searchbar

--- view/template.php
+++ view/template.php
@@ -11,104 +11,105 @@
 </head>
 
 <body>
-<!-- ---------------------------------NAVBAR--------------------------------- -->
-    <header>
-        <nav>
-            <div id="navtitle">
-                <a href="index.php?action=homePage">
-                    <i id="navlogo" class="fa-solid fa-clapperboard"></i>
-                    CINEMA DB
-                </a>
+    <div id="wrapper">
+
+    <!-- ---------------------------------NAVBAR--------------------------------- -->
+        <header>
+            <nav>
+                <div id="navtitle">
+                    <a href="index.php?action=homePage">
+                        <i id="navlogo" class="fa-solid fa-clapperboard"></i>
+                        CINEMA DB
+                    </a>
+                </div>
+
+                <div id="search">
+                    <input type="text" placeholder="search" id="searchbar"></input>
+                    <a id="searchButton" href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div> 
+                    
+
+                <button id="burger">
+                    <i id="menuIcon" class="fa-solid fa-bars"></i>
+                    <i id="closeIcon" class="fa-solid fa-xmark"></i>
+                </button>
+
+                <ul id="menu" class="hideMenu">
+
+                    <li>
+                        <a class="menuItem" href="index.php?action=homePage">HOME</a>
+                    </li>                
+                    <li>
+                        <a class="menuItem" href="index.php?action=listMovies">MOVIES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listDirectors">DIRECTORS</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listActors">ACTORS</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listRoles">ROLES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="index.php?action=listGenres">GENRES</a>
+                    </li>
+                    <li>
+                        <a class="menuItem" href="">CONTACT US</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    <!-- ----------------------------------MAIN---------------------------------- -->
+        <main>
+            <div id="content">
+                <?= $content ?>
+            </div>
+        </main>
+
+    <!-- ---------------------------------FOOTER--------------------------------- -->
+        <footer>
+            <div id="footer1">
+                <h2>CONTACT</h2>
+                <p>
+                    <a href="mailto:cinemadb@contact.com">cinemadb@contact.com</a>
+                </p>
+
+                <p>
+                    06 83 00 69 42
+                </p>  
             </div>
 
-            <div id="search">
-                <input type="text" placeholder="search" id="searchbar"></input>
-                <button id="searchbutton">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </div> 
-                
-
-            <button id="burger">
-                <i id="menuIcon" class="fa-solid fa-bars"></i>
-                <i id="closeIcon" class="fa-solid fa-xmark"></i>
-            </button>
-
-            <ul id="menu" class="hideMenu">
-
-                <li>
-                    <a class="menuItem" href="index.php?action=homePage">HOME</a>
-                </li>                
-                <li>
-                    <a class="menuItem" href="index.php?action=listMovies">MOVIES</a>
-                </li>
-                <li>
-                    <a class="menuItem" href="index.php?action=listDirectors">DIRECTORS</a>
-                </li>
-                <li>
-                    <a class="menuItem" href="index.php?action=listActors">ACTORS</a>
-                </li>
-                <li>
-                    <a class="menuItem" href="index.php?action=listRoles">ROLES</a>
-                </li>
-                <li>
-                    <a class="menuItem" href="index.php?action=listGenres">GENRES</a>
-                </li>
-                <li>
-                    <a class="menuItem" href="">CONTACT US</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-<!-- ----------------------------------MAIN---------------------------------- -->
-    <main id="wrapper">
-        <div id="content">
-            <?= $content ?>
-        </div>
-    </main>
-
-<!-- ---------------------------------FOOTER--------------------------------- -->
-    <footer>
-        <div id="footer1">
-            <h2>CONTACT</h2>
+            <div id="footer2">
             <p>
-                <a href="mailto:cinemadb@contact.com">cinemadb@contact.com</a>
+                    <a href="">Q&A</a>
             </p>
 
             <p>
-                06 83 00 69 42
-            </p>  
-        </div>
-
-        <div id="footer2">
-           <p>
-                <a href="">Q&A</a>
-           </p>
-
-           <p>
-                <a href="">Privacy policy</a>
-           </p>
-        </div>
-
-        <div id="footer3">
-            <h2>FOLLOW US</h2>
-
-            <p>
-                <span id="insta" class="socialIcon">
-                    <a href=""><i class="fa-brands fa-instagram"></i></a>
-                </span>
-
-                <span id="twitter" class="socialIcon">
-                    <a href=""><i class="fa-brands fa-square-twitter"></i></a>
-                </span>
-
-                <span id="fb" class="socialIcon">
-                    <a href=""><i class="fa-brands fa-square-facebook"></i></a>
-                </span>
+                    <a href="">Privacy policy</a>
             </p>
-        </div>
-    </footer>
+            </div>
+
+            <div id="footer3">
+                <h2>FOLLOW US</h2>
+
+                <p>
+                    <span id="insta" class="socialIcon">
+                        <a href=""><i class="fa-brands fa-instagram"></i></a>
+                    </span>
+
+                    <span id="twitter" class="socialIcon">
+                        <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+                    </span>
+
+                    <span id="fb" class="socialIcon">
+                        <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+                    </span>
+                </p>
+            </div>
+        </footer>
+    </div>
 
 <!-- ---------------------------SCRIPT MENU BURGER--------------------------- -->
     <script>
