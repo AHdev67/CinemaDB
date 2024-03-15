1a593d594429e70271606ccea120be3260b0984e
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-15 16:53:09 +0100

    added data preloading in modification form

--- view/modpage/modMovie.php
+++ view/modpage/modMovie.php
@@ -1,6 +1,7 @@
 <?php
     ob_start();
     $movie = $queryModForm->fetch();
+    $movieCategorization = $queryMovieCategorization->fetch();
 ?>
 
 <div class="container">
@@ -9,7 +10,7 @@
         <span>(fields with * are mandatory)</span>
     </div>
 
-    <a class="return" href="index.php?action=infoMovie&id=<?= $movie["id_film"] ?>">< Return to Movie</a>
+    <a class="return" href="index.php?action=infoMovie&id=<?= $movie["id_film"] ?>">< Return to movie</a>
 
     <form class="customForm" action="index.php?action=submitMovie" method="post">
 
@@ -21,7 +22,7 @@
         <p class="formBasicField">
             <label for="directorInput">Director* :</label>
             <select name="inputDirector" id="directorInput">
-                <option value="" disabled selected>Select director</option>
+                <option value="<?= $movie["id_realisateur"]?>" disabled selected><?= $movie["realisateurFilm"]?></option>
                 <?php
                 foreach($queryInputDirector->fetchALL() as $director) { ?>
                     <option value="<?= $director["id_realisateur"]?>"><?= $director["realisateurFilm"] ?></option>
@@ -31,20 +32,25 @@
         
         <p class="formBasicField">
             <label for="releasedateInput">Release date* :</label>
-            <input type="date" name="inputReleaseDate" id="releasedateInput">
+            <input type="date" name="inputReleaseDate" id="releasedateInput" value="<?= $movie["date_sortie"]?>">
         </p>
        
         <p class="formDuration">
             <label for="durationInput">Duration* :</label>
-            <input type="text" name="inputDuration" id="durationInput">
+            <input type="text" name="inputDuration" id="durationInput" value="<?= $movie["duree"]?>">
             minutes
         </p>
         
         <span class="strayTitle">Genre* :</span><br>
         <?php
-        foreach($queryInputGenre->fetchALL() as $genre) { ?>
+        foreach($queryInputGenre->fetchALL() as $genre) { 
+            if($genre["id_genre"] = $movieCategorization["id_genre"]){
+                $isChecked="checked";
+            }else{
+                $isChecked="";
+            }?>
             <p class="formGenre">
-                <input type="checkbox" value="<?= $genre["id_genre"]?>" name="<?= "inputGenre".$genre["id_genre"] ?>" id="<?= $genre["id_genre"] ?>">
+                <input type="checkbox" <?= $isChecked?> value="<?= $genre["id_genre"]?>" name="<?= "inputGenre".$genre["id_genre"] ?>" id="<?= $genre["id_genre"] ?>">
                 <label for="<?= $genre["id_genre"] ?>"><?= $genre["nom_genre"] ?></label>
             </p>
         <?php } ?>
@@ -52,6 +58,7 @@
         <p class="formScore">
              <label for="scoreInput">Score :</label>
             <select name="inputScore" id="scoreInput">
+                <option value="<?= $movie["note"]?>" disabled selected><?= $movie["note"]?></option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -63,12 +70,12 @@
        
         <p class="formBigText">
             <label for="synopsisInput">Synopsis :</label>
-            <textarea name="inputSynopsis" id="synopsisInput" cols="43" rows="10"></textarea>
+            <textarea name="inputSynopsis" id="synopsisInput" cols="43" rows="10"><?= $movie["synopsis"]?></textarea>
         </p>
         
         <p class="formBasicField">
             <label for="posterInput">Poster URL :</label>
-            <input type="url" name="inputPoster" id="posterInput">
+            <input type="url" name="inputPoster" id="posterInput" value="<?= $movie["affiche"]?>">
         </p>
         
         <input type="submit" name="submitForm" id="formSubmit" value="Add movie to DB" class="formSubmit">
